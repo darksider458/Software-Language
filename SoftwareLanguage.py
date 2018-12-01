ecd6269756bb1b7e97bfb723dacd4b7eb5824f5d
SecoWWW <patrik.scensny@gmail.com>
2018-12-01 17:20:20 +0100

    Seco modified files

--- SoftwareLanguage.py
+++ SoftwareLanguage.py
@@ -3,7 +3,7 @@ import sys, shlex, operator
 import re
 
 
-tk_http, tk_ftp, tk_telnet, tk_mailto,tk_divider, tk_question,tk_at, tk_doubledot, tk_dot, tk_plus, tk_space, tk_string,tk_letter,tk_number, tk_EOI = range(14)
+tk_http, tk_ftp, tk_telnet, tk_mailto,tk_divider, tk_question,tk_at, tk_doubledot, tk_dot, tk_plus, tk_space, tk_string,tk_letter,tk_number, tk_EOI = range(15)
 
 synbols = { '@':tk_at, '+':tk_plus, '/':tk_divider, ':':tk_doubledot, '.':tk_dot, '%':tk_space,'?':tk_question }
 keywords = {'http://':tk_http , 'ftp://':tk_ftp , 'telnet://':tk_telnet, 'mailto::':tk_mailto }
---
+++ simpleURL.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+import sys, shlex, operator
+import re
+import string
+
+tk_http, tk_ftp, tk_telnet, tk_mailto,tk_divider, tk_question,tk_at, tk_doubledot, tk_dot, tk_plus, tk_space, tk_string, tk_letter, tk_number, tk_EOI = range(15)
+
+symbols = { '@':tk_at, '+':tk_plus, '/':tk_divider, ':':tk_doubledot, '.':tk_dot, '%':tk_space,'?':tk_question }
+keywords = {'http://':tk_http , 'ftp://':tk_ftp , 'telnet://':tk_telnet, 'mailto::':tk_mailto }
+letter = list(string.ascii_letters)
+number = [str(i) for i in range(0,10)]
+
+print(number)
+
+
+
+# table = { ():[]
+
+
+
+# }
+
+
+
+def lexicalAnalysis(url):
+    pass
+
+
+def openFile():
+    with open('mock.txt', 'r') as samples:
+        for line in samples:            
+            lexicalAnalysis(line)
+
+if __name__ == "__main__":    
+    openFile()    
